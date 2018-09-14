f5cb845c3ec22d435fe40867f55a7de633d44379
Agnes Köhler <agnes.koehler@codecentric.de>
2018-09-14 17:16:35 +0200

    style cards to scale to 100%; add missing butterfly image

--- MemoryGame.js
+++ MemoryGame.js
@@ -42,7 +42,7 @@ var Level = function (evt, rows, cols, matches) {
     "use strict";
 
     var cardsList           = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW[\\]^_`abcdefghijklmnopqrstuvwxyzÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜ',
-        animalList           = ['bird', 'cat', 'cow', 'crocodile', 'dog', 'donkey', 'elephant', 'frog', 'giraffe', 'goat', 'horse', 'lion', 'monkey', 'mouse', 'rabbit', 'sheep', 'snake', 'spider'],
+        animalList           = ['bird', 'butterfly', 'cat', 'cow', 'crocodile', 'dog', 'donkey', 'elephant', 'frog', 'giraffe', 'goat', 'horse', 'lion', 'monkey', 'mouse', 'rabbit', 'sheep', 'snake', 'spider'],
         playfieldWrapper    = document.getElementById('playfield-wrapper'),
         playfield           = document.createElement('div'),
         cards               = [],
@@ -264,6 +264,7 @@ var MemoryGame = function (evt) {
         info        = document.getElementById('game-info'),
         lvlCtrls    = document.getElementById('levels'),
         lvls        = [
+            {'rows': 5, 'cols': 8, 'matches': 2},
             {'rows': 2, 'cols': 2, 'matches': 2},
             {'rows': 2, 'cols': 3, 'matches': 2},
             {'rows': 3, 'cols': 3, 'matches': 2},
@@ -273,7 +274,6 @@ var MemoryGame = function (evt) {
             {'rows': 5, 'cols': 5, 'matches': 2},
             {'rows': 5, 'cols': 6, 'matches': 2},
             {'rows': 5, 'cols': 7, 'matches': 2},
-            {'rows': 5, 'cols': 8, 'matches': 2}
         ],
         lastBtn     = lvlCtrls.childNodes[1],
         btn         = null,
